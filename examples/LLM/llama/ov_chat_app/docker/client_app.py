eac399ff553d94bc46e4762a82300c7181823ee4
likholat <anna.likholat@gmail.com>
2024-06-20 06:49:31 -0700

    async SDXL inference

--- examples/LLM/llama/ov_chat_app/docker/client_app.py
+++ examples/LLM/llama/ov_chat_app/docker/client_app.py
@@ -4,6 +4,9 @@ from concurrent.futures import ThreadPoolExecutor
 
 import requests
 import streamlit as st
+import asyncio
+import aiohttp
+import datetime
 
 import numpy as np
 from PIL import Image
@@ -33,7 +36,8 @@ with st.sidebar:
             st.session_state.model_sd_loaded = True
             st.success("Proceed to entering your prompt input!", icon="👉")
         else:
-            st.warning(f"Model {MODEL_NAME_SD} not loaded in TorchServe", icon="⚠️")
+            st.warning(
+                f"Model {MODEL_NAME_SD} not loaded in TorchServe", icon="⚠️")
     except requests.ConnectionError:
         st.warning("TorchServe is not up. Try again", icon="⚠️")
 
@@ -42,30 +46,65 @@ with st.sidebar:
 
 prompt = st.text_input("Text Prompt", "An astronaut riding a horse")
 
-#TODO: For Tests, delete when LLM added
+# TODO: For Tests, delete when LLM added
 prompt = [prompt,
           "A robot playing a violin",
           "A dragon flying over the mountains",
           "A mermaid painting a sunset on the beach"
-        ]
+          ]
+
 
 def generate_sd_response_v1(prompt_input):
     url = f"http://127.0.0.1:8080/predictions/{MODEL_NAME_SD}"
     response = []
     for pr in prompt_input:
-        data = json.dumps(
+        data_input = json.dumps(
             {
                 "prompt": pr
             }
         )
-        response.append(requests.post(url=url, data=data).text)
+        response.append(requests.post(url=url, data=data_input).text)
     return response
+
+
+async def send_inference_request(session, prompt_input):
+    url = f"http://127.0.0.1:8080/predictions/{MODEL_NAME_SD}"
+
+    data_input = json.dumps(
+        {
+            "prompt": prompt_input
+        }
+    )
+
+    async with session.post(url, data=data_input) as response:
+        assert response.status == 200
+        resp_text = await response.text()
+        return resp_text
+
+
+async def generate_sd_response_v2(prompts):
+    my_timeout = aiohttp.ClientTimeout(total=None)
+    async with aiohttp.ClientSession(timeout=my_timeout) as session:
+        tasks = []
+        for prompt in prompts:
+            tasks.append(send_inference_request(session, prompt))
+
+        return await asyncio.gather(*tasks)
+
 
 def response_postprocess(response):
     return [Image.fromarray(np.array(json.loads(text), dtype="uint8")) for text in response]
 
+# if st.button("Generate Images"):
+#     with st.spinner('Generating images...'):
+#         res = generate_sd_response_v1(prompt)
+#         images = response_postprocess(res)
+#         st.image(images, caption=["Generated Image"] * len(images), use_column_width=True)
+
+
 if st.button("Generate Images"):
     with st.spinner('Generating images...'):
-        res = generate_sd_response_v1(prompt)
+        res = asyncio.run(generate_sd_response_v2(prompt))
         images = response_postprocess(res)
-        st.image(images, caption=["Generated Image"] * len(images), use_column_width=True)
+        st.image(images, caption=["Generated Image"]
+                 * len(images), use_column_width=True)
